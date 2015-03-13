1d714c8875c2c27cccb5f2cad54801d1262b4f6f
jorgope <jorgope@gmail.com>
2015-03-13 17:20:06 +0000

    commit inicial

---
+++ tema0/ejemplo.c
@@ -0,0 +1,11 @@
+/*
+	Comentario multilinea
+*/
+#include  <stdio.h>
+int main (int argc, char *argv[]) 
+{
+// Comentario una linea
+	char mensaje[]="Mi primer programa C";
+	printf ("Este es %s.\n", mensaje);
+	return 0;	
+}
